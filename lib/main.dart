745f931a04818672a646222837536d89697378b7
Oliver <ocben1@gmail.com>
2020-12-19 12:10:39 +1100

    refactor for samsung bug

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
 void main() => runApp(MyApp());
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
+  // #docregion build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,59 +20,21 @@ class MyApp extends StatelessWidget {
       home: RandomWords(),
     );
   }
+// #enddocregion build
 }
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-class _RandomWordsState extends State<RandomWords> {
-  void _pushSaved() {
-    Navigator.of(context).push(
-      MaterialPageRoute<void> (
-        builder: (BuildContext context) {
-          final tiles = _saved.map(
-              (WordPair pair) {
-                return ListTile(
-                  title: Text(
-                    pair.asPascalCase,
-                    style: _biggerFont,
-                  ),
-                );
-              },
-          );
-          final divided = ListTile.divideTiles(
-              context: context,
-              tiles: tiles,
-          ).toList();
+// #enddocregion MyApp
 
-          return Scaffold(
-            appBar: AppBar(
-              title: Text('Saved Suggestions'),
-            ),
-            body: ListView(children: divided),
-          );
-        },
-      ),
-    );
-  }
+// #docregion RWS-var
+class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _saved = Set<WordPair>();
+  final _saved = <WordPair>{};
   final _biggerFont = TextStyle(fontSize: 18.0);
-  @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-     appBar:  AppBar(
-       title: Text('Startup Name Generator'),
-       actions: [
-         IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-       ],
-     ),
-     body: _buildSuggestions(),
-   );
-  }
+  // #enddocregion RWS-var
+
+  // #docregion _buildSuggestions
   Widget _buildSuggestions() {
     return ListView.builder(
-      padding:EdgeInsets.all(16.0),
+        padding: EdgeInsets.all(16.0),
         itemBuilder: /*1*/ (context, i) {
           if (i.isOdd) return Divider(); /*2*/
 
@@ -80,6 +45,9 @@ class _RandomWordsState extends State<RandomWords> {
           return _buildRow(_suggestions[index]);
         });
   }
+  // #enddocregion _buildSuggestions
+
+  // #docregion _buildRow
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
@@ -102,5 +70,58 @@ class _RandomWordsState extends State<RandomWords> {
       },
     );
   }
-}
+  // #enddocregion _buildRow
 
+  // #docregion RWS-build
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+  // #enddocregion RWS-build
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+                (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, //...to here.
+      ),
+    );
+  }
+// #docregion RWS-var
+}
+// #enddocregion RWS-var
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
